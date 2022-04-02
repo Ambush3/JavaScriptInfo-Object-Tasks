ec1d572911db3b1d0d858c638221bb885a1ca965
Aaron Bush <81124911+Ambush3@users.noreply.github.com>
2022-04-02 19:48:49 -0400

    Update JavaScriptInfo-Objects.js

--- JavaScriptInfo-Objects.js
+++ JavaScriptInfo-Objects.js
@@ -40,14 +40,14 @@ alert(sum);
 
 // Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
 
-// For instance:
-
 // before the call
 let menu = {
     width: 200,
     height: 300,
     title: "My menu"
 };
+
+
 function multiplyNumeric(obj) {
     for (let key in obj) {
         if (typeof obj[key] === 'number') {
